827ef725164685755a36d6fd9939149610b07f37
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2022-06-23 14:54:36 +0530

    Create booleanToString.js

---
+++ booleanToString.js
@@ -0,0 +1,25 @@
+function booleanToString(b){
+  let words = [b.toString()[0], b.toString()[1], b.toString[2], b.toString[3], b.toString()[4]];
+  if(words[0] == 't')
+      for(let i in words)
+        while(words[0] = "t")
+          while(words[1] = "r")
+            while(words[2] = "u")
+              while(words[3] = "e")
+                return words.splice(0, 20, "t", "r", "u", "e").join("");
+  if(words[0] == 'f')
+      for(let i in words)
+        while(words[0] = "f")
+          while(words[1] = "a")
+            while(words[2] = "l")
+              while(words[3] = "s")
+                while(words[4] = "e")
+                return words.splice(0, 20, "f", "a", "s", "s", "e").join("");
+  if(words[0] == 'd')
+      for(let i in words)
+        while(words[0] = "d")
+          while(words[1] = "u")
+            while(words[2] = "d")
+              while(words[3] = "e")
+                return words.splice(0, 20, "d", "u", "d", "e").join("");
+}
